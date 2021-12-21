02561b127d9603eca352630a1a682128c8b1ea8d
毛小翠 <9367953+mao-xiaocui@user.noreply.gitee.com>
2021-12-21 14:34:06 +0800

    首页tab 我的tab

--- am-app/src/views/manager/Home.vue
+++ am-app/src/views/manager/Home.vue
@@ -2,21 +2,101 @@
  * @Description: 主页tab
  * @Author: charles
  * @Date: 2021-12-14 20:42:55
- * @LastEditors: charles
- * @LastEditTime: 2021-12-14 21:28:58
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-21 14:20:24
 -->
 <template>
   <div class="home">
     <van-nav-bar title="首页" />
+    <div class="order_list">
+      <div class="order" v-for="o in workOrderData.list" :key="o.id">
+        <div class="time">{{o.create_time}}</div>
+        <van-row>
+          <van-col span="5" style="text-align:center;font-size:48px;position:bottom">
+            <van-icon name="https://img01.yzcdn.cn/vant/logo.png" />
+          </van-col>
+          <van-col span="18">
+            <div>类型： {{o.type}} </div>
+            <div>状态： {{o.status}} </div>
+            <div>设备： {{o.device_name}} </div>
+            <div>工程： {{o.engineer_name}} </div>
+            <div>创建时间: {{o.create_time}} </div>
+          </van-col>
+        </van-row>
+        <div class="btns">
+          <van-button plain hairline size="mini" type="primary" @click="toTakeOrderHandler(o.id)">接单</van-button>
+          <van-button plain hairline size="mini" type="primary" @click="toTakeOrderHandler(o.id)">拒绝接单</van-button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import {get} from '../../http/axios'
+import {mapState} from 'vuex'
+import {Toast} from 'vant'
 export default {
- 
+  data(){
+    return {
+      params:{
+        page:1,
+        pageSize:10,
+        status:'待接单'
+      },
+      workOrderData:{list:[]}
+    }
+  },
+  computed: {
+    ...mapState('user',['info'])
+  },
+  mounted() {
+    this.loadWorkOrder();
+  },
+  methods:{
+    // 接单
+    toTakeOrderHandler(id){
+      // 1. 获取工单id和用户id
+      let account_id = this.info.id;
+      let params = {id,account_id}
+      // 2. 调用接口完成接单
+      let url = "/workorder/takeOrder"
+      get(url,params).then(resp => {
+        // 3. 提示
+        Toast.success(resp.message)
+        this.loadWorkOrder();
+      })
+    },
+    loadWorkOrder(){
+      let url = "/workorder/pageQuery"
+      get(url,this.params).then(resp => {
+        this.workOrderData = resp.data;
+      })
+    }
+  }
 }
 </script>
 <style scoped>
 .home {
+  font-size: 12px;
+  color: #666;
   padding-bottom: 50px;
+  background-color: #f5f3f3;
+}
+.home .order_list{
+  padding: .5em;
+}
+.home .order_list > .order {
+  background-color: rgb(163, 219, 228);
+  margin-bottom: .5em;
+  border-radius: 5px;
+  padding:.5em
+}
+.home .order_list > .order > .time {
+  line-height: 2.4em;
+  border-bottom: 1px solid #f4f4f4;
+}
+.home .order_list > .order > .btns {
+  padding: .5em;
+  text-align: right;
 }
 </style>
--- am-app/src/views/manager/User.vue
+++ am-app/src/views/manager/User.vue
@@ -2,27 +2,46 @@
  * @Description: 我的tab
  * @Author: charles
  * @Date: 2021-12-14 20:42:55
- * @LastEditors: charles
- * @LastEditTime: 2021-12-14 21:25:44
+ * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-12-21 14:17:09
 -->
 <template>
   <div class="user">
     <div class="header">
       <div class="photo">
-        <img src="../../assets/user.png" alt=""/>
+        <img src="../../assets/mxc.jpg" alt=""/>
       </div>
       <div class="name">{{info.name}}</div>
     </div>
-    
+    <van-cell-group>
+      <van-cell title="昵称" value="花开富贵" />
+      <van-cell title="性别" value="女"  />
+      <van-cell title="电话号码" value="18410010086" />
+      <van-cell title="选择出生日期" :value="date" @click="show = true" />
+      <van-calendar v-model="show" @confirm="onConfirm" />
+    </van-cell-group>
     <van-cell title="所有订单" is-link to="/manager/order" />
-
-    <div class="btn" @click="logoutHandler"> 退出 </div>
+    <div class="btn" @click="logoutHandler"> 退出登录 </div>
+    <div class="btn" @click="logoutHandler"> 切换账号 </div>
   </div>
 </template>
 <script>
 import {mapState, mapActions} from 'vuex'
 export default {
+  data() {
+    return {
+      date: '',
+      show: false,
+    };
+  },
   methods:{
+     formatDate(date) {
+      return `${date.getMonth() + 1}/${date.getDate()}`;
+    },
+    onConfirm(date) {
+      this.show = false;
+      this.date = this.formatDate(date);
+    },
     ...mapActions('user',['logout']),
     logoutHandler(){
       this.logout();
@@ -44,11 +63,11 @@ export default {
 
 .header .photo {
   margin: 0 auto;
-  width: 10em;
-  height: 10em;
-  border-radius: 50%;
+  width: 20em;
+  height: 20em;
+  border-radius: 100%;
   box-sizing: border-box;
-  border:1px solid #1659a0;
+  border:5px solid #1659a0;
   overflow:hidden;
   padding: 1em;
 }
@@ -60,7 +79,7 @@ export default {
 }
 .header .photo img {
  width: 100%;
- border-radius: 50%;
+ border-radius: 100%;
 }
 
 .btn {
@@ -68,8 +87,10 @@ export default {
   margin: 1em auto;
   line-height: 3em;
   text-align: center;
-  border: 1px solid #ededed;
+  border: 1px solid #16408f;
   border-radius: 1.5em;
+  background: #5c95ce;
+  color: rgb(36, 95, 75);
 }
 
 </style>
