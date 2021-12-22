5f7de1e6b5e330447355c4908b15dff2a04754bf
IUbleuming <9368003+iubleuming@user.noreply.gitee.com>
2021-12-22 15:08:39 +0800

    [zwq]app

--- am-app/src/views/manager/Complete.vue
+++ am-app/src/views/manager/Complete.vue
@@ -1,7 +1,7 @@
 <!--
  * @Author: your name
  * @Date: 2021-12-21 11:35:05
- * @LastEditTime: 2021-12-22 15:07:24
+ * @LastEditTime: 2021-12-22 15:08:25
  * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: \group-2\am-app\src\views\manager\Complete.vue
@@ -38,7 +38,7 @@ export default {
     submitHandler(){
       let imgList = this.imgList;
       if(imgList.length < 3 ){
-        Toast.fail('必须上传3张图片!')
+        Toast.fail('必须提交3张图片!')
         return false;
       }
       let url = "/workorder/finishOrder"
